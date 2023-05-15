fbc4e7c59bb8bf3a7ab13b9aa4ae5781cccc36cd
Samson Akanbi <samson.akanbi@outlook.com>
2023-05-15 23:17:48 +0100

    Project Initialization.

---
+++ src/test/java/com/samseen/security/SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samseen.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
