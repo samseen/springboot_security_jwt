4225bd9a4922704dd882f4e48d7b6584fc30b886
Samson Akanbi <samson.akanbi@outlook.com>
2023-05-20 20:30:40 +0100

    Created the full register function in the service.

--- src/main/java/com/samseen/security/auth/AuthenticationService.java
+++ src/main/java/com/samseen/security/auth/AuthenticationService.java
@@ -1,14 +1,38 @@
 package com.samseen.security.auth;
 
+import com.samseen.security.config.JwtService;
+import com.samseen.security.user.Role;
+import com.samseen.security.user.User;
+import com.samseen.security.user.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class AuthenticationService {
+    
+    private final UserRepository repository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
 
     public AuthenticationResponse register(RegisterRequest request) {
-        return null;
+
+        User user = User.builder()
+                .firstName(request.getFirstname())
+                .lastName(request.getLastname())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(Role.USER)
+                .build();
+
+        repository.save(user);
+
+        String jwtToken = jwtService.generateToken(user);
+
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
     }
 
     public AuthenticationResponse authenticate(AuthenticationRequest request) {
